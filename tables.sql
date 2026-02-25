a1f8b5373e14eecd3b34a033ddb292814009f859
giacomodelucchi <giacomodelucchi@hotmail.it>
2026-02-25 10:31:07 +0100

    fix finali
    - correzione colore link breadcrumb
    - modifica tabella sql con nuovi ordini

--- tables.sql
+++ tables.sql
@@ -1,4 +1,4 @@
--- USE nomedb; -- inserite il vostro nome utente
+USE gdelucch;
 
 DROP TABLE IF EXISTS domanda_contattaci;
 DROP TABLE IF EXISTS item_allergico;
@@ -111,10 +111,10 @@ INSERT INTO item (id, tipo ,nome, descrizione, prezzo, immagine, testo_alternati
 
 INSERT INTO ordine (id, ritiro, ordinazione, persona, nome, cognome, telefono, annotazioni, stato, totale ) VALUES 
 (1, '2024-12-19 12:30:00', '2024-12-18 15:30:00', 'user', 'user', 'user', '3456789012', 'aggiungere una candelina', 4, 99.00),
-(2, '2024-12-19 13:15:00', '2024-12-18 16:45:00', 'user', 'user', 'user', '3456789012', NULL, 4, 49.50),
-(3, '2024-12-19 14:45:00', '2024-12-18 17:30:00', 'user', 'user', 'user', '3456789012', 'voglio delle decorazioni al cioccolato', 4, 53.00),
+(2, '2026-03-19 13:15:00', '2026-02-25 16:45:00', 'user', 'user', 'user', '3456789012', NULL, 4, 49.50),
+(3, '2026-04-22 12:00:00', '2026-02-25 19:55:37', 'user', 'user', 'user', '3456789012', 'voglio delle decorazioni al cioccolato', 4, 53.00),
 (4, '2024-12-20 09:30:00', '2024-12-19 18:35:00', 'admin', 'admin', 'admin', '3456789345', NULL, 1, 40.00),
-(5, '2024-12-20 11:00:00', '2024-12-19 19:55:37', 'admin', 'admin', 'admin', '3456759345', NULL, 1, 23.00);
+(5, '2026-03-25 11:00:00', '2026-02-25 19:55:37', 'admin', 'admin', 'admin', '3456759345', NULL, 1, 23.00);
 
 INSERT INTO ordine_torta (torta, ordine, porzioni, targa, numero_torte) VALUES 
 (1, 1, 8, 'Auguri Sara', 1),
